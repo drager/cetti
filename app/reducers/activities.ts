0df961719b9a88105ba2aed926eb659cca4ad1c5
Jesper Håkansson <jesper@jesperh.se>
2015-11-24 20:27:22 +0100

    feat: Added StackFrame entity

--- app/reducers/activities.ts
+++ app/reducers/activities.ts
@@ -1,21 +1,9 @@
 import { Activity } from '../entites';
-import { ADD_ACTIVITY, MARK_ACTIVITY_AS_RESOLVED } from '../action-types';
+import { MARK_ACTIVITY_AS_RESOLVED } from '../action-types';
 import initialState from '../initial-state';
 import { createReducer } from '../utils';
 
 const activities = {
-  [ADD_ACTIVITY]: (state: Activity[], action): Activity[] => {
-    return [
-      ...state,
-      {
-        id: action.payload.id,
-        title: action.payload.title,
-        timesOccurred: action.payload.timesOccurred,
-        timeOfOccurence: action.payload.timeOfOccurence,
-        resolved: false,
-      },
-    ];
-  },
   [MARK_ACTIVITY_AS_RESOLVED]: (state: Activity[], action): Activity[] => {
     return state.map(activity =>
       activity.id === action.activity.id

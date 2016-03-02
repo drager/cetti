e4350fe655de87e25415fa84d1b22f6d5106edfa
Rasmus Eneman <pie.or.paj@gmail.com>
2016-03-02 14:24:59 +0100

    Remove travis stuff

--- ui/app/index.tsx
+++ ui/app/index.tsx
@@ -1,13 +1,6 @@
-/* tslint:disable:no-unused-variable */
 import * as React from 'react';
-/* tslint:enable:no-unused-variable */
-
-// Temporary disable lint warning here. Waiting for tslint 2.5.2...
-
 import { render } from 'react-dom';
 import { Router, Route } from 'react-router';
-
-import { getBuilds } from './travis-builds';
 
 import { Main } from './components/main';
 import { Dashboard } from './components/dashboard';
@@ -27,5 +20,3 @@ render(
   ),
   document.getElementById('app')
 );
-
-getBuilds();
